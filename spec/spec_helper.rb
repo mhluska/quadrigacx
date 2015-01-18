056f5973c8852858dd5dd86b1872a0fc675232e2
Maros Hluska <mhluska@gmail.com>
2015-01-18 12:45:12 -0500

    Move fixtures to spec directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,6 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
